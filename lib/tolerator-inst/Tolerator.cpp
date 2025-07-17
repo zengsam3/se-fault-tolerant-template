7be41951cb45d7da4a3f606bc7c2cff5df7e1fb7
Sam Zeng* <bsz1@sfu.ca>
2025-07-17 03:05:54 -0700

    task 1 code

--- lib/tolerator-inst/Tolerator.cpp
+++ lib/tolerator-inst/Tolerator.cpp
@@ -47,6 +47,72 @@ Tolerator::runOnModule(Module& m) {
         FunctionType::get(Builder.getVoidTy(), false)
     );
 
+  for (Function &F : m) {
+        if (F.isDeclaration()) continue;
+        SmallVector<Instruction*, 32> WorkList;
+        for (BasicBlock &BB : F)
+            for (Instruction &I : BB)
+                if (isa<LoadInst>(&I)
+                 || isa<StoreInst>(&I)
+                 || (isa<CallInst>(&I)
+                     && cast<CallInst>(&I)->getCalledFunction()
+                     && cast<CallInst>(&I)->getCalledFunction()->getName() == "free")
+                 || (isa<BinaryOperator>(&I)
+                     && (cast<BinaryOperator>(&I)->getOpcode() == Instruction::SDiv
+                      || cast<BinaryOperator>(&I)->getOpcode() == Instruction::UDiv
+                      || cast<BinaryOperator>(&I)->getOpcode() == Instruction::SRem
+                      || cast<BinaryOperator>(&I)->getOpcode() == Instruction::URem)))
+                    WorkList.push_back(&I);
+
+        for (Instruction *I : WorkList) {
+            BasicBlock *OrigBB = I->getParent();
+            BasicBlock *ContBB = OrigBB->splitBasicBlock(I, OrigBB->getName() + ".cont");
+            OrigBB->getTerminator()->eraseFromParent();
+            BasicBlock *ErrBB = BasicBlock::Create(Ctx, OrigBB->getName() + ".err", OrigBB->getParent(), ContBB);
+
+            Builder.SetInsertPoint(ErrBB);
+            if (dyn_cast<LoadInst>(I))
+                Builder.CreateCall(H_read);
+            else if (dyn_cast<StoreInst>(I))
+                Builder.CreateCall(H_write);
+            else if (CallInst *CI = dyn_cast<CallInst>(I))
+                if (CI->getCalledFunction() && CI->getCalledFunction()->getName() == "free")
+                    Builder.CreateCall(H_free);
+                else
+                    Builder.CreateCall(H_div0);
+            else
+                Builder.CreateCall(H_div0);
+            Builder.CreateUnreachable();
+
+            Builder.SetInsertPoint(OrigBB);
+            Value *Cond = nullptr;
+            if (LoadInst *LI = dyn_cast<LoadInst>(I)) {
+                Cond = Builder.CreateICmpEQ(
+                    LI->getPointerOperand(),
+                    Constant::getNullValue(LI->getPointerOperand()->getType())
+                );
+            } else if (StoreInst *SI = dyn_cast<StoreInst>(I)) {
+                Cond = Builder.CreateICmpEQ(
+                    SI->getPointerOperand(),
+                    Constant::getNullValue(SI->getPointerOperand()->getType())
+                );
+            } else if (CallInst *CI = dyn_cast<CallInst>(I)) {
+                Value *Ptr = CI->getArgOperand(0);
+                Cond = Builder.CreateICmpNE(
+                    Ptr,
+                    Constant::getNullValue(Ptr->getType())
+                );
+            } else {
+                BinaryOperator *BOp = cast<BinaryOperator>(I);
+                Value *Divisor = BOp->getOperand(1);
+                Cond = Builder.CreateICmpEQ(
+                    Divisor,
+                    ConstantInt::get(Divisor->getType(), 0)
+                );
+            }
+            Builder.CreateCondBr(Cond, ErrBB, ContBB);
+        }
+    }
 
   return true;
 }
