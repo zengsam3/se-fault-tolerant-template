d8e0b2b218acb69f39b84f9a844a14ed99058697
Sam Zeng* <bsz1@sfu.ca>
2025-07-17 04:23:57 -0700

    dwa

--- lib/tolerator-inst/Tolerator.cpp
+++ lib/tolerator-inst/Tolerator.cpp
@@ -8,6 +8,7 @@
 #include "llvm/IR/Constants.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Passes/PassBuilder.h"
+#include "llvm/ADT/SmallVector.h"
 #include "Tolerator.h"
 
 
@@ -48,77 +49,74 @@ Tolerator::runOnModule(Module& m) {
     );
 
   for (Function &F : m) {
-    StringRef name = F.getName();
-    if (F.isDeclaration()) continue;
-    if (name.starts_with("handle_invalid_")) continue;
-    for (BasicBlock &BB : F) {
-      SmallVector<Instruction*, 8> WorkList;
-      for (Instruction &I : BB) {
-        if (isa<LoadInst>(&I) || isa<StoreInst>(&I)) {
-          WorkList.push_back(&I);
-        } else if (CallInst *CI = dyn_cast<CallInst>(&I)) {
-          if (Function *CF = CI->getCalledFunction();
-              CF && CF->getName() == "free")
-            WorkList.push_back(&I);
-        } else if (BinaryOperator *BO = dyn_cast<BinaryOperator>(&I)) {
-          unsigned opc = BO->getOpcode();
-          if (opc == Instruction::SDiv ||
-              opc == Instruction::UDiv ||
-              opc == Instruction::FDiv)
-            WorkList.push_back(&I);
-        }
-      }
-      for (auto it = WorkList.rbegin(); it != WorkList.rend(); ++it) {
-        Instruction *I = *it;
-        BasicBlock *OrigBB = I->getParent();
-        BasicBlock *ContBB = OrigBB->splitBasicBlock(I, OrigBB->getName() + ".cont");
-        OrigBB->getTerminator()->eraseFromParent();
-        BasicBlock *ErrBB = BasicBlock::Create(Ctx, OrigBB->getName() + ".err", OrigBB->getParent(), ContBB);
+        if (F.isDeclaration()) continue;
+        SmallVector<Instruction*, 32> WorkList;
+        for (BasicBlock &BB : F)
+            for (Instruction &I : BB)
+                if (isa<LoadInst>(&I)
+                 || isa<StoreInst>(&I)
+                 || (isa<CallInst>(&I)
+                     && cast<CallInst>(&I)->getCalledFunction()
+                     && cast<CallInst>(&I)->getCalledFunction()->getName() == "free")
+                 || (isa<BinaryOperator>(&I)
+                     && (cast<BinaryOperator>(&I)->getOpcode() == Instruction::SDiv
+                      || cast<BinaryOperator>(&I)->getOpcode() == Instruction::UDiv
+                      || cast<BinaryOperator>(&I)->getOpcode() == Instruction::SRem
+                      || cast<BinaryOperator>(&I)->getOpcode() == Instruction::URem)))
+                    WorkList.push_back(&I);
 
-        IRBuilder<> ErrB(ErrBB);
-        if (isa<LoadInst>(I))
-          ErrB.CreateCall(H_read);
-        else if (isa<StoreInst>(I))
-          ErrB.CreateCall(H_write);
-        else if (dyn_cast<CallInst>(I))
-          ErrB.CreateCall(H_free);
-        else
-          ErrB.CreateCall(H_div0);
-        ErrB.CreateUnreachable();
+        for (Instruction *I : WorkList) {
+            BasicBlock *OrigBB = I->getParent();
+            BasicBlock *ContBB = OrigBB->splitBasicBlock(I, OrigBB->getName() + ".cont");
+            OrigBB->getTerminator()->eraseFromParent();
+            BasicBlock *ErrBB = BasicBlock::Create(Ctx, OrigBB->getName() + ".err", OrigBB->getParent(), ContBB);
 
-        IRBuilder<> CondB(OrigBB);
-        Value *Cond = nullptr;
-        if (LoadInst *LI = dyn_cast<LoadInst>(I)) {
-          Cond = CondB.CreateICmpEQ(
-            LI->getPointerOperand(),
-            Constant::getNullValue(LI->getPointerOperand()->getType()));
-        } else if (StoreInst *SI = dyn_cast<StoreInst>(I)) {
-          Cond = CondB.CreateICmpEQ(
-            SI->getPointerOperand(),
-            Constant::getNullValue(SI->getPointerOperand()->getType()));
-        } else if (CallInst *CI = dyn_cast<CallInst>(I)) {
-          Value *Ptr = CI->getArgOperand(0);
-          Cond = CondB.CreateICmpEQ(
-            Ptr,
-            Constant::getNullValue(Ptr->getType()));
-        } else {
-          BinaryOperator *BO = cast<BinaryOperator>(I);
-          Value *Divisor = BO->getOperand(1);
-          if (BO->getOpcode() == Instruction::FDiv)
-            Cond = CondB.CreateFCmpUEQ(
-              Divisor,
-              ConstantFP::get(Divisor->getType(), 0.0));
-          else
-            Cond = CondB.CreateICmpEQ(
-              Divisor,
-              ConstantInt::get(Divisor->getType(), 0));
+            Builder.SetInsertPoint(ErrBB);
+            if (dyn_cast<LoadInst>(I))
+                Builder.CreateCall(H_read);
+            else if (dyn_cast<StoreInst>(I))
+                Builder.CreateCall(H_write);
+            else if (CallInst *CI = dyn_cast<CallInst>(I))
+                if (CI->getCalledFunction() && CI->getCalledFunction()->getName() == "free")
+                    Builder.CreateCall(H_free);
+                else
+                    Builder.CreateCall(H_div0);
+            else
+                Builder.CreateCall(H_div0);
+            Builder.CreateUnreachable();
+
+            Builder.SetInsertPoint(OrigBB);
+            Value *Cond = nullptr;
+            if (LoadInst *LI = dyn_cast<LoadInst>(I)) {
+                Cond = Builder.CreateICmpEQ(
+                    LI->getPointerOperand(),
+                    Constant::getNullValue(LI->getPointerOperand()->getType())
+                );
+            } else if (StoreInst *SI = dyn_cast<StoreInst>(I)) {
+                Cond = Builder.CreateICmpEQ(
+                    SI->getPointerOperand(),
+                    Constant::getNullValue(SI->getPointerOperand()->getType())
+                );
+            } else if (CallInst *CI = dyn_cast<CallInst>(I)) {
+                Value *Ptr = CI->getArgOperand(0);
+                Cond = Builder.CreateICmpNE(
+                    Ptr,
+                    Constant::getNullValue(Ptr->getType())
+                );
+            } else {
+                BinaryOperator *BOp = cast<BinaryOperator>(I);
+                Value *Divisor = BOp->getOperand(1);
+                Cond = Builder.CreateICmpEQ(
+                    Divisor,
+                    ConstantInt::get(Divisor->getType(), 0)
+                );
+            }
+            Builder.CreateCondBr(Cond, ErrBB, ContBB);
         }
-        CondB.CreateCondBr(Cond, ErrBB, ContBB);
-      }
     }
-  }
 
   return true;
 }
 
-static RegisterPass<tolerator::Tolerator> X("tolerator", "Tolerator Pass", false, false);
+
+
